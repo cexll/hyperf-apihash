846ed28cfe7161bbc99e58b29b658720049ef101
cexll <evanxian9@gmail.com>
2021-07-23 09:05:59 +0800

    Update ConfigProvider.php

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -14,7 +14,7 @@ class ConfigProvider
                     'id' => 'config',
                     'description' => 'The RSA public and private certificate path is configured here',
                     'source' => __DIR__ . '/../publish/config/apihash.php',
-                    'destination' => BASE_PATH . '/config/autoload/tcc.php',
+                    'destination' => BASE_PATH . '/config/autoload/apihash.php',
                 ]
             ]
         ];
